9cf27a8c2a0fdb36ece6aeb3afbaf39e0e173141
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-12-03 20:48:24 +0530

    values ERC20 Ownable

---
+++ src/TokenProgram2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract TokenProgram2 is ERC20, Ownable {
+    constructor() ERC20("Abhishek", "AB") Ownable(msg.sender) {
+        _mint(msg.sender, 10000000000);
+    }
+
+    function mintTo(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+}
