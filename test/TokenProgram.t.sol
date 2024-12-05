faabae80000c07663efdefafd966457c372738aa
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-12-05 23:00:39 +0530

    made changestotest token program val

--- test/TokenProgram.t.sol
+++ test/TokenProgram.t.sol
@@ -3,17 +3,20 @@ pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
 import { TokenProgram2 } from "../src/TokenProgram2.sol";
+import { TokenProgram } from "../src/TokenProgram.sol";
 
 contract TokenProgramTest is Test {
-    TokenProgram2 tokenProgram;
+    TokenProgram2 tokenProgram2;
+    TokenProgram tokenProgram;
 
     function setUp() external {
-        tokenProgram = new TokenProgram2();
+        tokenProgram2 = new TokenProgram2();
+        tokenProgram = new TokenProgram();
     }
 
     function testDeployment() public view {
-        assertEq(tokenProgram.name(), "Abhishek");
-        assertEq(tokenProgram.symbol(), "ABETH");
+        assertEq(tokenProgram2.name(), "Abhishek");
+        assertEq(tokenProgram2.symbol(), "ABETH");
     }
 
 }
